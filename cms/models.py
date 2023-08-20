635b69320f6ced3d39a046924ae5cc8e5966a754
Sandil Ranasinghe <sandilsranasinghe@gmail.com>
2023-08-20 11:49:40 +0530

    sample models

--- cms/models.py
+++ cms/models.py
@@ -1,4 +1,28 @@
-from django.db import models
 from django.contrib.auth.models import User
+from django.db import models
 
 # Create your models here.
+
+
+class Ship(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Planet(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Booking(models.Model):
+    ship = models.ForeignKey(Ship, on_delete=models.CASCADE)
+    planet = models.ForeignKey(Planet, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateField()
+
+    def __str__(self):
+        return f"{self.ship} to {self.planet} on {self.date}"
